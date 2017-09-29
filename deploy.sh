47e19b86a0ded7ce51f1ab0b21dd898d55cba526
chenxianlong <794465731@qq.com>
2017-09-29 15:00:48 +0800

    create-blog event-1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source_path="$PWD/source/_posts"
+commit_msg=
 
 file_names=$(find $source_path -type f -name "*.md" -mtime -1)
 
@@ -15,8 +16,27 @@ done
 hexo clean \
 && hexo deploy \
 && echo -e "\033[32m =====deploy successed===== \033[0m" \
-&& echo -e "\033[33m =====will push commit to repository===== \033[0m" \
-&& sleep 3s \
-&& git add . \
-&& git commit -m "${new_files[@]}" \
-&& git push git@github.com:xxxxxMiss/xxxxxMiss.github.io.git master:gh-pages
+&& echo -e "\033[33m =====will push source code to repository===== \033[0m" \
+
+select option in "Yes" "No"; do
+  case $option in
+    Yes )
+    git add .
+
+    if [[ -z ${#new_files[@]} ]]; then
+      read -r -p "Enter a commit msg" msg
+      commit_msg=$msg
+    else
+      commit_msg=${new_files[@]}
+    fi
+
+    git commit -m "${commit_msg}" \
+    && git push git@github.com:xxxxxMiss/xxxxxMiss.github.io.git master:gh-pages
+    ;;
+    No )
+    echo -e "\033[32m Bye! \033[0m"
+    exit 0
+    ;;
+  esac
+done
+
